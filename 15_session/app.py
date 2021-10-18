93631b022ebe91166301347c068bf7e710b7a627
RoshaniS5 <roshanishrestha@Roshanis-MacBook-Air.local>
2021-10-18 19:08:55 -0400

    modified k15

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+# Selective Soup | Justin Zou (Piggy), Wen Hao Dong (Jal Hordan), Roshani Shrestha (Pete)
+# SoftDev 
+# K15: Sessions Greetings - Uses session capabilities to allow a user to login and logout 
+# 2021-10-18
 
-app = Flask(__name__)
+from sys import argv
+from flask import Flask, render_template, request
 
-@app.route("/")
-def root():
-    pass
+app = Flask(__name__)    
 
-if __name__ == "__main__":
-    app.debug = True
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage(): 
+   return render_template( 'login.html' ) 
+
+@app.route("/auth") 
+def authenticate():  
+    if (request.args['username'] == 'Selective Soup' and request.args.get('Password')== 'hello'):
+        return render_template('response.html', username=request.args['username'], password=request.args.get('Password'))
+    else:
+        return render_template('login.html')
+   
+
+
+    
+if __name__ == "__main__": 
+    app.debug = True 
     app.run()
