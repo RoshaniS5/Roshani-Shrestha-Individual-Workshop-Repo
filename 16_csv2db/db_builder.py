dd422d4a2eff9479061e4bfb0661106ca8702cf3
RoshaniS5 <roshanishrestha@Roshanis-MacBook-Air.local>
2021-10-24 12:43:05 -0400

    added notes, comments, headings

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,7 +1,7 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Dec 2020 -- The Time of the Rona
+# Selective Soup | Justin Zou (Piggy), Wen Hao Dong (Jal Hordan), Roshani Shrestha (Pete)
+# SoftDev 
+# K16: All About Database - skeleton/stub :: SQLITE3 BASICS 
+# 2021-10-25
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -14,16 +14,47 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
+# -- courses.csv
 
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+# Creates the table with the correct column names and types
+c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)")
 
+with open("courses.csv", "r") as courses_file: # opens courses.csv
+    # reads the csv file and converts each row into a dictionary, name of the column is set equal to the value at the row
+    reader = csv.DictReader(courses_file) 
+    for row in reader:
+        # f-strings were used to make commands more readable
+        # instead of using string concatenation
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+        # The repr() function, when called with a string,
+        # automatically adds quotes around them.
+        command = f"""
+            INSERT INTO courses VALUES
+              ({repr(row['code'])},
+               {row['mark']},
+               {row['id']})
+        """
+        # print("***DIAG: command ***") 
+        # print(command)
+        c.execute(command) 
+
+# -- students.csv
+c.execute("CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)") 
+
+with open("students.csv", "r") as students_file: # opens students.csv
+    reader = csv.DictReader(students_file) 
+    for row in reader:
+        command = f"""
+            INSERT INTO students VALUES
+              ({repr(row['name'])},
+               {row['age']},
+               {row['id']})
+        """
+        # print("***DIAG: command ***") 
+        # print(command)
+        c.execute(command) 
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
