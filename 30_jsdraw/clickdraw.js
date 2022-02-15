1efebd694fe05e2d74855c290b46b115a5be1508
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-02-14 22:47:47 -0500

    update k30

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -18,42 +18,59 @@ var mode = "rect";
 // var toggleMode = function(e) {
 var toggleMode = (e) => {
     console.log("toggling...");
-    if (e) {
-        ctx.strokeStyle = 'red';
+    var toggle = document.getElementById("buttonToggle");
+    if (mode === "rect") {
+        mode = "circ";
+        toggle.innerHTML = "circle";
     }
     else {
-        ctx.strokeStyle = 'red';
+        mode = "rect";
+        toggle.innerHTML = "rectangle";
     }
 }
 
-var drawRect() = function(e) {
-    var mouseX = offsetX;
-    var mouseY = offsetY;
+var drawRect = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.strokeStyle = 'red';
+    ctx.fillStyle = 'red';
     ctx.beginPath();
+    ctx.fillRect(mouseX, mouseY, 50, 150);
 }
 
 //var drawCircle = function(e) {
 var drawCircle = (e) => {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    console.log("mouseclick registered at ", mouseX, mouseY);
+    ctx.strokeStyle = 'red';
+    ctx.fillStyle = 'red';
     ctx.beginPath();
     ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
     ctx.stroke();
-    console.log("mouseclick registered at ", mouseX, mouseY);
     ctx.fill();
 }
 
 //var draw = function(e) {
 var draw = (e) => {
     console.log("draw");
+    if (mode === "rect") {
+        drawRect(e);
+    }
+    else {
+        drawCircle(e);
+    }
 }
 
 //var wipeCanvas = function() {
 var wipeCanvas = () => {
-    clearRect();
+    console.log("wipe")
+    ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
 }
 
 c.addEventListener("click", draw);
-var bToggler = document. ;
-bToggler. ;
-var clearB = ;
-clearB. ; 
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener("click", toggleMode);
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas); 
